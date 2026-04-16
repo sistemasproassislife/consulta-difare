581873ce2fe3effd2b8e16104ac5603fe4e89065
sistemasproassislife <sistemas.proassismed@gmail.com>
2026-04-16 12:13:50 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,11 @@
+export const metadata = {
+  title: "Consulta Difare",
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
